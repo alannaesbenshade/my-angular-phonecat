8c12ce91912c042a910c6720bb1a19698ceca26b
alannaesbenshade <alannaesbenshade@cedarville.edu>
2017-11-17 00:20:57 -0500

    Continued tutorial

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -6,14 +6,43 @@ describe('phoneList', function() {
   beforeEach(module('phoneList'));
 
   // Test the controller
-  describe('PhoneListController', function() {
+  describe('controller', function() {
+    it('should create a `phones` property with 2 phones fetched with `$http`', function() {
+  expect(ctrl.phones).toBeUndefined();
 
-    it('should create a `phones` model with 3 phones', inject(function($componentController) {
-      var ctrl = $componentController('phoneList');
+  it('should set a default value for the `orderProp` property', function() {
+  expect(ctrl.orderProp).toBe('age');
+});
 
-      expect(ctrl.phones.length).toBe(3);
-    }));
+  $httpBackend.flush();
+  expect(ctrl.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+});
+     var $httpBackend, ctrl;
+
+     // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+     // This allows us to inject a service and assign it to a variable with the same name
+     // as the service while avoiding a name conflict.
+     beforeEach(inject(function($componentController, _$httpBackend_) {
+       $httpBackend = _$httpBackend_;
+       $httpBackend.expectGET('phones/phones.json')
+                   .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
+       ctrl = $componentController('phoneList');
+     }));
+
 
   });
 
+
 });
+
+
+    // describe('PhoneListController', function() {
+    //
+    //   it('should create a `phones` model with 3 phones', inject(function($componentController) {
+    //     var ctrl = $componentController('phoneList');
+    //
+    //     expect(ctrl.phones.length).toBe(3);
+    //     }));
+    //
+    //   });
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,7 +2,6 @@
 
 // Angular E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
-
 describe('PhoneCat Application', function() {
 
   describe('phoneList', function() {
@@ -11,18 +10,26 @@ describe('PhoneCat Application', function() {
       browser.get('index.html');
     });
 
-    it('should filter the phone list as a user types into the search box', function() {
-      var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+    // it('should filter the phone list as a user types into the search box', function() {
+    //   var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+    //   var query = element(by.model('$ctrl.query'));
+    //
+    //   expect(phoneList.count()).toBe(3);
+    //
+    //   query.sendKeys('nexus');
+    //   expect(phoneList.count()).toBe(1);
+    //
+    //   query.clear();
+    //   query.sendKeys('motorola');
+    //   expect(phoneList.count()).toBe(2);
+    // });
+
+    it('should render phone specific links', function() {
       var query = element(by.model('$ctrl.query'));
-
-      expect(phoneList.count()).toBe(3);
-
       query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
 
-      query.clear();
-      query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(2);
+      element.all(by.css('.phones li a')).first().click();
+      expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
     });
 
   });
